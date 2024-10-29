23289ac9ce1754b6ae148e412bfc2834b8b8ba95
dell <spmanoj2000@gmail.com>
2024-10-29 03:42:30 +0000

    modified windows change 2

--- python-linux.py
+++ python-linux.py
@@ -1,2 +1,2 @@
-print("this is my first python scripts in linux modified by manoj in windows")
+print("this is my first python scripts in linux modified by manoj in windows -change2")
 print("this is my first python scripts in linux modified by manoj ubuntu vm- change 2")
